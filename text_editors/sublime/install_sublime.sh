a3527ed882a78e926384593c569b988da78eccbe
Jacob <Drost011@umn.edu>
2015-02-13 09:17:46 -0600

    Added sublime text 2 support

---
+++ text_editors/sublime/install_sublime.sh
@@ -0,0 +1,14 @@
+###############
+# Install Atom
+###############
+sudo add-apt-repository ppa:webupd8team/sublime-text-2
+sudo apt-get update
+sudo apt-get install sublime-text
+
+###############
+# Install Packages
+###############
+
+###############
+# Change Settings
+###############
